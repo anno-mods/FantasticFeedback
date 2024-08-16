d4d661cf666d25507382de60df6a261a95233fb3
taubenangriff <51975164+taubenangriff@users.noreply.github.com>
2024-08-16 17:46:14 +0200

    Fix incorrect channel loading

--- FeedbackEditor/ViewModel/Timeline/FeedbackConfigViewModel.cs
+++ FeedbackEditor/ViewModel/Timeline/FeedbackConfigViewModel.cs
@@ -68,6 +68,7 @@ namespace FeedbackEditor.ViewModel
                 viewModel.ChannelName = "Unnamed Sequence " + index;
                 Childs.Add(viewModel);
                 SequenceDefinitions.Add(viewModel);
+                index++;
             }
 
             GuidVariations = new ObservableCollection<GuidVariation>(
